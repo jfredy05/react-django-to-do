762cbc3f81ff3907928c3eae26fc75610365cac2
Fredy Garcia <fredy.garcia@roiback.com>
2022-02-10 20:42:14 -0500

    Add frontend and Add App Django

---
+++ frontend/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './App.css';
+
+/* function App() {
+  return (
+    <div className="container">
+    </div>
+  );
+} */
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+
+        <div id="task-container">
+          <div  id="form-wrapper">
+             <form id="form">
+              <div className="flex-wrapper">
+                <div style={{flex: 6}}>
+                  <input onChange="" className="form-control" id="title" value="" type="text" name="title" placeholder="Add task.." />
+                </div>
+
+                <div style={{flex: 1}}>
+                  <input id="submit" className="btn btn-warning" type="submit" name="Add" />
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
